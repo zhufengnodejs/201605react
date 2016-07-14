880c6266c3ff2bc9c3382cc5303b853400668a2d
zhufengnodejs <zhufengnodejs@126.com>
2016-07-14 17:59:10 +0800

    保存到的localStorage

--- js/14.board.js
+++ js/14.board.js
@@ -1,17 +1,18 @@
 var Board = React.createClass({
     //定义初始状态
     getInitialState:function(){
-        return {messages:["今天好热呀!"]};
+        return {messages:this.props.messages};
     },
     handleClick:function(event){
         event.target.value = '';
     },
     addMsg:function(){
-        //var message = this.refs.txtMsg.value;
-        var message = document.querySelector('#txtMsg').value;
+        var message = this.refs.txtMsg.value;
+        //var message = document.querySelector('#txtMsg').value;
         var messages = this.state.messages;//先取出原来的数组
         messages.push(message);//向此数组中添加新的留言
         // setState是一个异步方法,如果要获取设置之后的最新的state值要在回调函数中获取
+        localStorage.setItem('messages',JSON.stringify(messages));
         this.setState({messages:messages},function(){
            // console.log(this.state.messages)
         });//设置状态
@@ -20,10 +21,10 @@ var Board = React.createClass({
         return <div className="panel panel-success">
             <div className="panel-heading">
                 <div className="row">
-                    <div className="col-xs-8">
-                        <input type="text" defaultValue={this.props.msg} className="form-control" id="txtMsg" onClick={this.handleClick} />
+                    <div className="col-xs-10">
+                        <input type="text" defaultValue={this.props.msg} className="form-control" ref="txtMsg" onClick={this.handleClick} />
                     </div>
-                    <div className="col-xs-4">
+                    <div className="col-xs-2">
                         <button onClick={this.addMsg} className="btn btn-primary">留言</button>
                     </div>
                 </div>
@@ -40,8 +41,13 @@ var Board = React.createClass({
         </div>
     }
 });
+//读取localStorage的值
+var data = {
+    msg:'请留言',
+    messages: localStorage.getItem('messages')?JSON.parse(localStorage.getItem('messages')):[]
+}
 
 ReactDOM.render(
-    <Board msg="请留言"/>,
+    <Board {...data}/>,
     document.querySelector('#app')
 )
