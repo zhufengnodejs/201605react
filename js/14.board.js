8c6eeae3749c15898a2561e730f303ce3c528462
zhufengnodejs <zhufengnodejs@126.com>
2016-07-14 17:49:19 +0800

    绘制留言版界面

--- js/14.board.js
+++ js/14.board.js
@@ -3,15 +3,28 @@ var Board = React.createClass({
     getInitialState:function(){
         return {messages:["今天好热呀!"]};
     },
+    handleClick:function(event){
+        event.target.value = '';
+    },
+    addMsg:function(){
+        //var message = this.refs.txtMsg.value;
+        var message = document.querySelector('#txtMsg').value;
+        var messages = this.state.messages;//先取出原来的数组
+        messages.push(message);//向此数组中添加新的留言
+        // setState是一个异步方法,如果要获取设置之后的最新的state值要在回调函数中获取
+        this.setState({messages:messages},function(){
+           // console.log(this.state.messages)
+        });//设置状态
+    },
     render:function(){
         return <div className="panel panel-success">
             <div className="panel-heading">
                 <div className="row">
                     <div className="col-xs-8">
-                        <input type="text" className="form-control"/>
+                        <input type="text" defaultValue={this.props.msg} className="form-control" id="txtMsg" onClick={this.handleClick} />
                     </div>
                     <div className="col-xs-4">
-                        <button className="btn btn-primary">留言</button>
+                        <button onClick={this.addMsg} className="btn btn-primary">留言</button>
                     </div>
                 </div>
             </div>
@@ -29,6 +42,6 @@ var Board = React.createClass({
 });
 
 ReactDOM.render(
-    <Board/>,
+    <Board msg="请留言"/>,
     document.querySelector('#app')
 )
